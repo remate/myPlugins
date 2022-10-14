2818ac3e737bdfb9a76e2b0d0d0a2afc81a81975
zuoci <452642848@qq.com>
2022-10-14 10:51:20 +0800

    no message

--- cci-layout/layout-web/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export const isTestMode = process.env.NODE_ENV === 'test'
-export const isDevtMode = process.env.NODE_ENV === 'development'
-
-// 使用layout  不用时可用于微应用或者iframe中
-export const useLayout = true
